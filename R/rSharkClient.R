2d52c3980796ec7258910f0b91b0c786e5c13971
luqmanulkhair <luqmanulkhair@gmail.com>
2016-12-15 07:46:35 +0100

    loadDataLogical & writeData implemented

--- R/rSharkClient.R
+++ R/rSharkClient.R
@@ -1,6 +1,6 @@
 #' Create MongoDBUtils Function
 #'
-#' This function creates MongoDBUtil on backend.
+#' This function creates MongoDBUtils on backend.
 #' @name rShark.createMongoDBUtils 
 #' @param SparkSession Provided from outside with specific parameters.
 #' @return MongoDBUtils 
@@ -18,14 +18,77 @@ rShark.createMongoDBUtils <- function(sparkSession){
 #'
 #' This function calls loadData on backend.
 #' @name rShark.loadData
+#' @param mongoDBUtils To be provided on which loadData method is called.
 #' @param collectionName To be provided for which data is retrived from backend.
 #' @return SparkDataFrame 
 #' @keywords loadData
 #' @export
 #' @examples
-#' rShark.loadData (collectionName)
+#' rShark.loadData (mongoDBUtils,collectionName)
 
 rShark.loadData <- function(mongoDBUtils,collectionName){
   sparkDataFrame<- new("SparkDataFrame",sparkR.callJMethod(mongoDBUtils,"loadData",collectionName),FALSE) 
   sparkDataFrame
+}
+
+
+
+#' Creates list of logical lists Function
+#'
+#' This function creates List of type List on backend.
+#' @name rShark.createLogicalLists
+#' @param logLists list of vectors of strings.
+#' @return List<List>
+#' @keywords createLogicalLists
+#' @export
+#' @examples
+#' rShark.createLogicalLists (list(c("ProcessMetrics","ProductMetrics"), c("ID","Name"))
+
+rShark.createLogicalLists <- function(logLists){
+  logicalLists<-sparkR.newJObject("java.util.ArrayList")
+  for(logList in logLists){
+    logicalList<-sparkR.newJObject("java.util.ArrayList")
+    for(logValue in logList){
+      sparkR.callJMethod(logicalList,"add",logValue)
+    }
+    sparkR.callJMethod(logicalLists,"add",logicalList)
+  }
+  
+  logicalLists
+}
+
+
+#' Call loadDataLogical Function
+#'
+#' This function calls loadDataLogical on backend.
+#' @name rShark.loadDataLogical
+#' @param mongoDBUtils To be provided on which loadDataLogical method is called.
+#' @param collectionName To be provided for which data is retrived from backend.
+#' @param logicalLists List of type List to provided on which filtering is performed.
+#' @return SparkDataFrame 
+#' @keywords loadDataLogical
+#' @export
+#' @examples
+#' rShark.loadDataLogical (mongoDBUtils,collectionName,logicalLists)
+
+rShark.loadDataLogical <- function(mongoDBUtils,collectionName,logicalLists){
+  sparkDataFrame<- new("SparkDataFrame",sparkR.callJMethod(mongoDBUtils,"loadDataLogical",collectionName,logicalLists),FALSE) 
+  sparkDataFrame
+}
+
+
+#' Call writeData Function
+#'
+#' This function calls writeData on backend.
+#' @name rShark.writeData
+#' @param mongoDBUtils To be provided on which writeData method is called.
+#' @param dataFrame To be provided which is upserted in the Database.
+#' @param collectionName To be provided where Data will be upserted.
+#' @keywords writeData
+#' @export
+#' @examples
+#' rShark.writeData (mongoDBUtils,dataFrame,collectionName)
+
+rShark.writeData <- function(mongoDBUtils,dataFrame,collectionName){
+  sparkR.callJMethod(mongoDBUtils,"writeData",dataFrame,collectionName)
 }
